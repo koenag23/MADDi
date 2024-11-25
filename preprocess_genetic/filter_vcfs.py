4d1f21cbfa389331775f7abd0a7339c7c6eceea5
koenag <104934193+koenag@users.noreply.github.com>
2024-11-25 15:54:55 -0800

    Made edits to filter_vcfs.py

--- preprocess_genetic/filter_vcfs.py
+++ preprocess_genetic/filter_vcfs.py
@@ -1,7 +1,7 @@
 import io
 import os
-import pandas as pd
 import numpy as np
+import pandas as pd
 import gzip
 
 def get_vcf_names(vcf_path):
@@ -36,40 +36,54 @@ def main():
     
     
     genes = pd.read_csv("gene_list.csv")
-    files = os.listdir("[insert file]")
-    
+    # Example:
+    # files = os.listdir("vcf_dir")
+    files = os.listdir("YOUR_PATH_TO_VCFS")
     
     for vcf_file in files:
-        file_name = "[insert file]" + vcf_file
+        # Example:
+        # vcf_file = "ADNI.808_indiv.minGQ_21.pass.ADNI_ID.chr21.vcf.gz"
+        # file_name = "vcf_dir/ADNI.808_indiv.minGQ_21.pass.ADNI_ID.chr21.vcf.gz"
+        file_name = "YOUR_PATH_TO_VCFS" + vcf_file
         
         output_file = open('log.txt','a')
         output_file.write(file_name)
         output_file.close()
-        # names = get_vcf_names(file_name)
-        # vcf = pd.read_csv(file_name, compression='gzip', comment='#', chunksize=10000, delim_whitespace=True, header=None, names=names)
-        # vcf = pd.concat(vcf, ignore_index=True)
-        vcf = read_vcf(file_name)
-        print(vcf)
+        names = get_vcf_names(file_name)
+        vcf_chunks = pd.read_csv(file_name, compression='gzip', comment='#', chunksize=10000, delim_whitespace=True, header=None, names=names)
+
+        # To process each chunk at a time and add it to the data frame
+        # Note: currently set to only process first 11 chunks
+        # vcf = pd.DataFrame()
+        # for i, chunk in enumerate(vcf_chunks):
+        #     print(f"Processing chunk {i + 1}, shape: {chunk.shape}")
+        #     vcf = pd.concat([vcf, chunk], ignore_index=True)
+
+        #     if (i == 10):
+        #         break
+        # print("Final DataFrame shape:", vcf.shape)
+
+        vcf = pd.concat(vcf_chunks, ignore_index=True)
+
         
         start = vcf_file.find("ADNI_ID.") + len("ADNI_ID.")
-        end = vcf_file.find("output.vcf")
+        end = vcf_file.find(".vcf")
         substring = vcf_file[start:end]
         relevent = genes[genes["chrom"] == substring]
         relevent = relevent.reset_index()
-        
         positions = vcf["POS"]
         
         
         indexes = []
         for i in range(len(positions)):
-            
             boo = in_between(positions[i], relevent)
             if i % 500 == 0:
                 output_file = open('log.txt','a')
-                output_file.write(f"Position: {positions[i]}, Relevant: {boo}\n")
+                output_file.write(" " + str(boo) + " ")
                 output_file.close()
             if boo:
                 indexes.append(i)
+
         
         if len(indexes) != 0:
             df = vcf.iloc[indexes]
